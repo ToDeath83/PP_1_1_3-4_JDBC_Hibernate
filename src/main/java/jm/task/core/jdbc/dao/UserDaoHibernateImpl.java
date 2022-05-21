7729399ee6a78f36699e7aad86edf2460c553805
Sergey Katalymov <todeath83@mail.ru>
2022-05-21 14:02:19 +0300

    1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class UserDaoHibernateImpl extends Util implements UserDao {
     SessionFactory sessionFactory = getSessionFactory();
+    Session session;
 
     public UserDaoHibernateImpl() {
 
@@ -17,58 +18,78 @@ public class UserDaoHibernateImpl extends Util implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try (Session session = sessionFactory.getCurrentSession()) {
+        try {
+            session = sessionFactory.getCurrentSession();
             session.beginTransaction();
             session.createSQLQuery("CREATE TABLE IF NOT EXISTS Users" +
                     " (id INT PRIMARY KEY AUTO_INCREMENT, name varchar(255), lastName varchar(255), age int)").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
             session.getTransaction().commit();
         }
     }
 
     @Override
     public void dropUsersTable() {
-        try (Session session = sessionFactory.getCurrentSession()) {
+        try {
+            session = sessionFactory.getCurrentSession();
             session.beginTransaction();
             session.createSQLQuery("DROP TABLE IF EXISTS Users").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
             session.getTransaction().commit();
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try (Session session = sessionFactory.getCurrentSession()) {
+        try {
+            session = sessionFactory.getCurrentSession();
             session.beginTransaction();
             User user = new User(name, lastName, age);
             session.save(user);
             session.getTransaction().commit();
             System.out.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (Exception e) {
+            session.getTransaction().commit();
         }
     }
 
     @Override
     public void removeUserById(long id) {
-        try (Session session = sessionFactory.getCurrentSession()) {
+        try {
+            session = sessionFactory.getCurrentSession();
             session.beginTransaction();
             session.remove(session.get(User.class, id));
+            session.getTransaction().commit();
+        } catch (Exception e) {
             session.getTransaction().commit();
         }
     }
 
     @Override
     public List<User> getAllUsers() {
-        try (Session session = sessionFactory.getCurrentSession()) {
+        List<User> list = null;
+        try {
+            session = sessionFactory.getCurrentSession();
             session.beginTransaction();
-            List<User> list = session.createQuery("SELECT i FROM User i", User.class).getResultList();
+            list = session.createQuery("SELECT i FROM User i", User.class).getResultList();
             session.getTransaction().commit();
             return list;
+        } catch (Exception e) {
+            session.getTransaction().commit();
         }
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
-        try (Session session = sessionFactory.getCurrentSession()) {
+        try {
+            session = sessionFactory.getCurrentSession();
             session.beginTransaction();
             session.createQuery("delete User").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
             session.getTransaction().commit();
         }
     }
